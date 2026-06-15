333549ff26486e0bd60f95d26fdf0b5446c9b932
Beardy Dev <m.aisthorpe@hotmail.com>
2026-06-16 00:52:35 +1000

    Add C bridge for true co-op damage logging

---
+++ soh/soh/Enhancements/true-coop/TrueCoopBridge.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void TrueCoop_SetDebugEnabled(bool enabled);
+bool TrueCoop_IsDebugEnabled(void);
+
+void TrueCoop_LogEnemyDamageEventC(
+    int16_t sceneId,
+    int16_t roomId,
+    int16_t actorId,
+    int16_t actorCategory,
+    float positionX,
+    float positionY,
+    float positionZ,
+    int16_t rotationX,
+    int16_t rotationY,
+    int16_t rotationZ,
+    int16_t hpBefore,
+    int16_t damageAmount,
+    int16_t hpAfter,
+    bool died);
+
+#ifdef __cplusplus
+}
+#endif
