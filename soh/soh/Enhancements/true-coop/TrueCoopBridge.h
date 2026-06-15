e02351bf45f3612d5344aa3b4a83dd3daebfdf4f
Beardy Dev <m.aisthorpe@hotmail.com>
2026-06-16 01:06:01 +1000

    Expose true co-op enemy kill bridge

--- soh/soh/Enhancements/true-coop/TrueCoopBridge.h
+++ soh/soh/Enhancements/true-coop/TrueCoopBridge.h
@@ -26,6 +26,19 @@ void TrueCoop_LogEnemyDamageEventC(
     int16_t hpAfter,
     bool died);
 
+void TrueCoop_LogEnemyKillEventC(
+    int16_t sceneId,
+    int16_t roomId,
+    int16_t actorId,
+    int16_t actorCategory,
+    float positionX,
+    float positionY,
+    float positionZ,
+    int16_t rotationX,
+    int16_t rotationY,
+    int16_t rotationZ,
+    int16_t hpAtKill);
+
 #ifdef __cplusplus
 }
 #endif
