979b33f5bacc5bebb5faea76bd6dbe620d27a82d
Beardy Dev <m.aisthorpe@hotmail.com>
2026-06-16 01:06:22 +1000

    Implement true co-op enemy kill bridge

--- soh/soh/Enhancements/true-coop/TrueCoopBridge.cpp
+++ soh/soh/Enhancements/true-coop/TrueCoopBridge.cpp
@@ -43,3 +43,31 @@ void TrueCoop_LogEnemyDamageEventC(
 
     TrueCoop::LogEnemyDamageEvent(event);
 }
+
+void TrueCoop_LogEnemyKillEventC(
+    int16_t sceneId,
+    int16_t roomId,
+    int16_t actorId,
+    int16_t actorCategory,
+    float positionX,
+    float positionY,
+    float positionZ,
+    int16_t rotationX,
+    int16_t rotationY,
+    int16_t rotationZ,
+    int16_t hpAtKill) {
+    TrueCoop::EnemyKillEvent event;
+    event.enemy.sceneId = sceneId;
+    event.enemy.roomId = roomId;
+    event.enemy.actorId = actorId;
+    event.enemy.actorCategory = actorCategory;
+    event.enemy.positionX = positionX;
+    event.enemy.positionY = positionY;
+    event.enemy.positionZ = positionZ;
+    event.enemy.rotationX = rotationX;
+    event.enemy.rotationY = rotationY;
+    event.enemy.rotationZ = rotationZ;
+    event.hpAtKill = hpAtKill;
+
+    TrueCoop::LogEnemyKillEvent(event);
+}
